ee1177b664c9c3f0212501caf192e8566156e7d9
haoqiwang97 <haoqiwang970205@gmail.com>
2021-10-27 17:14:41 -0500

    build TrivialNN

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 import argparse
 from data import *
 from evaluate import *
+from models import *
+import numpy as np
+import time
+
 
 def _parse_args():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="TrivialNN", help="model to run")
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--num_epochs', type=int, default=10, help='number of epochs to train for')
+    parser.add_argument('--hidden_size', type=int, default=100, help='hidden layer size')
+    parser.add_argument('--batch_size', type=int, default=100, help='training batch size; 1 by default')
+    
+    parser.add_argument('--device', default="cuda:0")
+    args = parser.parse_args()
+    return args
 
 
-X_train, y_train, X_val, y_val, X_test, y_test = load_datasets()
-print("%i train exs, %i dev exs, %i test exs" % (len(y_train), len(y_val), len(y_test)))
-
-from sklearn.neural_network import MLPRegressor
-
-
-regr = MLPRegressor(random_state=1, hidden_layer_sizes=(32, 32))
-regr.fit(X_train, y_train)
-print_evaluation_all(y_val, regr.predict(X_val), y_train, regr.predict(X_train))
-"""
-{'rmse_train': 8.553915194554236,
- 'rmse_val': 12.080965850866708,
- 'r2_score_train': 0.6960283159879984,
- 'r2_score_val': 0.42546866914412207,
- 'mean_absolute_error_train': 2.4911227590600893,
- 'mean_absolute_error_val': 2.7781083526912753,
- 'mean_absolute_percentage_error_train': 3262853998109949.5,
- 'mean_absolute_percentage_error_val': 3456464339849024.0,
- 'median_absolute_error_train': 0.3542516706761658,
- 'median_absolute_error_val': 0.3574926208790601,
- 'explained_variance_score_train': 0.6961102419109616,
- 'explained_variance_score_val': 0.42561490845825445}
-"""
-
-X_train, y_train, X_val, y_val, X_test, y_test = load_datasets(is_remove_lon_lat=True)
-regr = MLPRegressor(random_state=1, hidden_layer_sizes=(5))
-regr.fit(X_train, y_train)
-print_evaluation_all(y_val, regr.predict(X_val), y_train, regr.predict(X_train))
-"""
-{'rmse_train': 10.815070874719089,
- 'rmse_val': 11.553563890343979,
- 'r2_score_train': 0.5140831170073712,
- 'r2_score_val': 0.4745367541493415,
- 'mean_absolute_error_train': 2.8799263228282603,
- 'mean_absolute_error_val': 2.9495983510488664,
- 'mean_absolute_percentage_error_train': 4074336238771302.5,
- 'mean_absolute_percentage_error_val': 4218080327931728.0,
- 'median_absolute_error_train': 0.6629017912484265,
- 'median_absolute_error_val': 0.6650102826496709,
- 'explained_variance_score_train': 0.5144109184899529,
- 'explained_variance_score_val': 0.47485922831677774}
-"""
+def evaluate(classifier, X_all, y_all):
+    X_all = torch.from_numpy(np.array(X_all)).float()
+    y_all = torch.from_numpy(np.array(y_all)).float().view((-1, 1))
+    
+    y_pred = np.zeros(len(X_all))
+    
+    for idx, X in enumerate(X_all):
+        y_pred[idx] = classifier.predict(X)
+        
+    return print_evaluation(y_all, y_pred)
+        
+if __name__ == '__main__':
+    args = _parse_args()
+    print(args)
+    
+    X_train, y_train, X_val, y_val, X_test, y_test = load_datasets()
+    print("%i train exs, %i dev exs, %i test exs" % (len(y_train), len(y_val), len(y_test)))
+    
+    # Train and evaluate
+    start_time = time.time()
+    if args.model == "TrivialNN":
+        model = train_trivialnn(args, X_train, y_train, X_val, y_val)
+    
+    model.to("cpu")
+    print("=====Train Accuracy=====")
+    train_eval = evaluate(model, X_train, y_train)
+    print(train_eval)
+    
+    print("=====Val Accuracy=====")
+    val_eval = evaluate(model, X_val, y_val)
+    print(val_eval)
+    
+    train_eval_time = time.time() - start_time
+    print("Time for training and evaluation: %.2f seconds" % train_eval_time)
+    
+    # TODO: gpu, batch size
--- models.py
+++ models.py
@@ -1,33 +1,97 @@
 from data import *
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklego.meta import ZeroInflatedRegressor
-from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch import optim
+import random
 
-df = read_data(is_small=False) # 'tx_crash.csv' the new feature does not seem useful, acc=0.815
 
-df_X = df.drop(columns=['tot_crash_count', 'crash'])
-df_y = df['crash']
+class TrivialNN(nn.Module):
+    def __init__(self, inp, hid, out):
+        """
+        Construct the computation graph
 
-X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.1, random_state=1)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=1)
+        Parameters
+        ----------
+        inp : int
+            size of input
+        hid : int
+            size of hidder layer
+        out : int
+            size of output
 
-zir = ZeroInflatedRegressor(
-    classifier=RandomForestClassifier(),
-    regressor=RandomForestRegressor()
-)
+        Returns
+        -------
+        None.
 
-zir.fit(X_train, y_train)
-y_val_pred = zir.predict(X_val)
-plt.plot(y_val, y_val_pred, '.')
-plt.show()
+        """
+        super(TrivialNN, self).__init__()
+        
+        self.fc1 = nn.Linear(inp, hid)
+        self.l1 = nn.ReLU()
+        self.fc2 = nn.Linear(hid, hid)
+        self.l2 = nn.ReLU()
+        self.fc3 = nn.Linear(hid, out)
+        
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.l1(out)
+        out = self.fc2(out)
+        out = self.l2(out)
+        out = self.fc3(out)
+        return out
+        
+    def predict(self, x):
+        with torch.no_grad():
+            return self.forward(x)
+            
 
-y_test_pred = zir.predict(X_test)
-plt.scatter(y_test, y_test_pred)
-plt.show()
-
-zir.score(X_train, y_train)
-zir.score(X_val, y_val)
-zir.score(X_test, y_test)
+def train_trivialnn(args, X_train, y_train, X_val, y_val):
+    X_train = torch.from_numpy(np.array(X_train)).float()
+    y_train = torch.from_numpy(np.array(y_train)).float().view((-1, 1))
+    
+    inp_size = X_train.shape[1]
+    hid_size = args.hidden_size
+    out_size = 1
+    
+    n_epochs = args.num_epochs
+    batch_size = args.batch_size
+    n_batches = int(len(X_train) / batch_size)
+    lr = args.lr
+    
+    device = args.device
+    trivialnn = TrivialNN(inp_size, hid_size, out_size)
+    trivialnn.to(device)
+    
+    optimizer = optim.Adam(trivialnn.parameters(), lr=lr)
+    loss_func = nn.MSELoss()
+    
+    for epoch in range(0, n_epochs):
+        ex_indices = [i for i in range(0, len(X_train))]
+        random.shuffle(ex_indices)
+        total_loss = 0.0
+        
+        for batch_idx in range(n_batches):
+            # print("batch_idx", batch_idx)
+            batch_ex_indices = ex_indices[batch_idx * batch_size: (batch_idx + 1) * batch_size]
+            batch_loss = 0.0
+            
+        # for idx in batch_ex_indices:
+        # for idx in ex_indices:
+            X = X_train[batch_ex_indices].to(device)
+            y = y_train[batch_ex_indices].to(device)
+            
+            trivialnn.zero_grad()
+            prediction = trivialnn.forward(X)
+            loss = loss_func(prediction, y)
+            
+            # batch_loss += loss
+            total_loss += loss
+            
+        # batch_loss /= batch_size
+        # batch_loss.backward() # compute gradients
+            loss.backward()
+            optimizer.step() # apply gradients
+        print("Total loss on epoch %i: %f" % (epoch, total_loss))
+        
+    return trivialnn
+                
